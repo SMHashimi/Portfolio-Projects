7697a31ea3bdcc727662cac55cce698c599b459e
SayedMubarisHashimi <mubaris.hasimi2018@gmail.com>
2024-10-28 02:41:08 -0400

    BandName

---
+++ Day01BandNameGenerator/BandNameGenerator.py
@@ -0,0 +1,10 @@
+
+print("Welcome to the Band Name Generator Application!")
+city_name = input("What is the city you grew up? ")
+pet_name = input(f"What is your pet name? ")
+def band_name_generator(city, pet):
+    band_name = f"Your band name: {city} {pet}".title()
+    return band_name
+band_name = band_name_generator(city_name, pet_name)
+print(band_name)
+
