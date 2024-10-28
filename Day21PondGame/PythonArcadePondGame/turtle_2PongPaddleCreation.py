c94990a415a00cb8ac3302b33f82bbc80b494925
SayedMubarisHashimi <mubaris.hasimi2018@gmail.com>
2024-10-27 23:51:47 -0400

    init

--- Day21PondGame/PythonArcadePondGame/turtle_2PongPaddleCreation.py
+++ Day21PondGame/PythonArcadePondGame/turtle_2PongPaddleCreation.py
@@ -2,14 +2,20 @@ from turtle_1PondScreenSetting import *
 
 screen.tracer(0)
 right_paddle = Turtle("square")
+left_paddle = Turtle("square")
 def right_paddle_creation_to_the_right_side(color):
     right_paddle.color(color)
     right_paddle.shapesize(stretch_len = 1, stretch_wid = 5)
     right_paddle.penup()
     right_paddle.goto(350, 0)
+def left_paddle_creation_to_the_right_side(color):
+    left_paddle.color(color)
+    left_paddle.shapesize(stretch_len = 1, stretch_wid = 5)
+    left_paddle.penup()
+    left_paddle.goto(-350, 0)
 
 right_paddle_creation_to_the_right_side("white")
-
+left_paddle_creation_to_the_right_side("white")
 
 
 
--- Day21PondGame/PythonArcadePondGame/turtle_3PongPaddleMoving.py
+++ Day21PondGame/PythonArcadePondGame/turtle_3PongPaddleMoving.py
@@ -4,23 +4,28 @@ def right_paddle_up():
     #the x-axis does not change
     y_movement = right_paddle.ycor() + 15
     right_paddle.goto(right_paddle.xcor(), y_movement)
-    print(y_movement)
-
 def right_paddle_down():
     #the x-axis does not change
     y_movement = right_paddle.ycor() - 20
     right_paddle.goto(right_paddle.xcor(), y_movement)
-    print(y_movement)
+def left_paddle_up():
+    #the x-axis does not change
+    y_movement = left_paddle.ycor() + 15
+    left_paddle.goto(left_paddle.xcor(), y_movement)
+def left_paddle_down():
+    #the x-axis does not change
+    y_movement = left_paddle.ycor() - 20
+    left_paddle.goto(left_paddle.xcor(), y_movement)
 
 screen.listen()
 screen.onkey(right_paddle_up, "Up")
 screen.onkey(right_paddle_down, "Down")
+screen.onkey(left_paddle_up, "w")
+screen.onkey(left_paddle_down, "s")
 
 game_is_on = True
 while game_is_on:
     screen.update()
-
-
 
 screen.exitonclick()
 
