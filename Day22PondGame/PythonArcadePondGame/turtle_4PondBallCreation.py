c53c9598046360867b6295e4c2100087d90e47f5
SayedMubarisHashimi <mubaris.hasimi2018@gmail.com>
2024-11-04 03:15:55 -0500

    adding collision to paddle as well as ball

--- Day22PondGame/PythonArcadePondGame/turtle_4PondBallCreation.py
+++ Day22PondGame/PythonArcadePondGame/turtle_4PondBallCreation.py
@@ -1,8 +1,8 @@
 from turtle_3PondPaddleMoving import *
-
 ball = Turtle()
 ball.shape("circle")
 ball.color("white")
+# ball.speed("fastest")
 
 # def ball_move():
 #      x_movement = ball.xcor() + 10
@@ -10,18 +10,4 @@ ball.color("white")
 #      ball.penup()
 #      ball.goto(x_movement, y_movement)
 #ball_move()
-
-x_move = 10
-y_move = 10
-
-def ball_move():
-     global x_move, y_move
-     ball.penup()
-     ball.goto(ball.xcor() + x_move, ball.ycor() + y_move)
-     if ball.ycor() > 290 or ball.ycor() < -290:
-          y_move *= -1
-     if ball.xcor() > 390 or ball.xcor() < -390:
-          x_move *= -1
-
-
 
--- Day22PondGame/PythonArcadePondGame/turtle_9PondBallBounce.py
+++ Day22PondGame/PythonArcadePondGame/turtle_9PondBallBounce.py
@@ -1,20 +1,11 @@
-from turtle_4PondBallCreation import *
+from turtle_5PondBallWallPaddleCollision import *
 import time
-
-
 
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-
-    ball_move()
+    ball_WallPaddleCollisionX_Y()
 
 screen.exitonclick()
-
-
-
-
-
-
 
 
