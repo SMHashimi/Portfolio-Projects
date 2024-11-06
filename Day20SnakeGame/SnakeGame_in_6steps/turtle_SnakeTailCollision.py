de727bb7c5e163bfb7bb037689110bdeca37b442
SayedMubarisHashimi <mubaris.hasimi2018@gmail.com>
2024-11-06 10:08:42 -0500

    Snake

---
+++ Day20SnakeGame/SnakeGame_in_6steps/turtle_SnakeTailCollision.py
@@ -0,0 +1,8 @@
+from turtle_SnakeScoreBoard_WallCollision import *
+
+# def collision_tail():
+#     for segment in turtles_list[1:]:
+#         if turtles_list[0].distance(segment) < 10:
+#             moving_snake = False
+#             score_board.goto(0, 0)
+#             score_board.write("GAME OVER", align="center", font="Courier")
