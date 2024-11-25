2c0686dcbba15318bc867025c674acf27a6e8cc4
SayedMubarisHashimi <mubaris.hasimi2018@gmail.com>
2024-11-25 00:33:52 -0500

    PasswordManager

--- Day28PasswordManager/main.py
+++ Day28PasswordManager/main.py
@@ -1,22 +1,38 @@
 from tkinter import *
 
-themanager_window = Tk()
-themanager_window.title("Password Manager")
-themanager_window.config(padx = 20, pady = 20)
+the_manager_window = Tk()
+the_manager_window.title("Password Manager")
+the_manager_window.config(padx = 50, pady = 50)
 
-themanager_canvas = Canvas(width = 200, height = 200)
-themanager_image = PhotoImage(file = "logo.png")
-themanager_canvas.create_image(100, 100, image = themanager_image)
+the_manager_canvas = Canvas(width = 200, height = 200)
+logo_image = PhotoImage(file = "logo.png")
+the_manager_canvas.create_image(100, 100, image = logo_image)
+the_manager_canvas.grid(column = 1, row = 0)
 
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+#making website label and entry
+website_label = Label(text = "Website:")
+website_label.grid(column = 0, row = 1)
+website_entry = Entry(width = 52)
+website_entry.grid(column = 1, row = 1, columnspan = 2)
 
+#making email label and entry
+email_Username = Label(text = "Email/Username:")
+email_Username.grid(column = 0, row = 2)
+email_entry = Entry(width = 52)
+email_entry.grid(column = 1, row = 2, columnspan = 2)
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+#making password label and entry
+password_label = Label(text = "Password:")
+password_label.grid(column = 0, row = 3)
+password_entry = Entry(width = 21)
+password_entry.grid(column = 1, row = 3)
 
-# ---------------------------- UI SETUP ------------------------------- #
-#
+# making generate password button
+generate_password = Button(text = "Generate Password")
+generate_password.grid(column = 2, row = 3)
 
+# making add button
+add_button = Button(text = "Add", width = 44)
+add_button.grid(column = 1, row = 4, columnspan = 2, pady = 5)
 
-
-themanager_canvas.grid()
-themanager_window.mainloop()
+the_manager_window.mainloop()
