50acda295f31fa8481226146559428597a7322f1
SayedMubarisHashimi <mubaris.hasimi2018@gmail.com>
2024-12-03 21:03:27 -0500

    SendingSMSOnWhatsApp

--- Day35SendingSMS/twilio_01_SendingMessageOnWhatsAppIfRaining.py
+++ Day35SendingSMS/twilio_01_SendingMessageOnWhatsAppIfRaining.py
@@ -1,9 +1,9 @@
 from requests import *
 from twilio.rest import Client
 
-ACCOUNT_SID = "ACaf558fa5a2e4b53d3e6c85ae8b6104a6"
-AUTH_TOKEN = "a35234df73788e44d0ed3377391b789e"
-ZAINAB_HASHIMI = "whatsapp:+13473408570"
+ACCOUNT_SID = "*********************"
+AUTH_TOKEN = "********************"
+ZAINAB_HASHIMI = "whatsapp:+1********"
 
 API_Key = "86c339ecb93847edd2a4f7ee7cd0dcf1"
 
